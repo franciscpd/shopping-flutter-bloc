3fa9797a076f3d313c626ea8bd251aa000b3195b
Franciscpd <franciscpd@gmail.com>
2019-08-27 13:15:12 -0300

    Add home page android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_bloc/blocs/home.block.dart';
-import 'package:shopping_bloc/ui/shared/widgets/category/category-list.widget.dart';
-import 'package:shopping_bloc/ui/shared/widgets/product/product-list.widget.dart';
+import 'package:shopping_bloc/ui/android/pages/home.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,50 +30,6 @@ class Main extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final HomeBloc bloc = Provider.of<HomeBloc>(context);
-
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(
-              height: 60,
-            ),
-            Text(
-              "Categorias",
-              style: Theme.of(context).textTheme.headline,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CategoryList(
-              categories: bloc.categories,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Mais Vendidos",
-              style: Theme.of(context).textTheme.headline,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ProductList(
-              products: bloc.products,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/ui/android/pages/home.page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_bloc/blocs/home.block.dart';
+import 'package:shopping_bloc/ui/shared/widgets/category/category-list.widget.dart';
+import 'package:shopping_bloc/ui/shared/widgets/product/product-list.widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final HomeBloc bloc = Provider.of<HomeBloc>(context);
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Categorias",
+              style: Theme.of(context).textTheme.headline,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CategoryList(
+              categories: bloc.categories,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Mais Vendidos",
+              style: Theme.of(context).textTheme.headline,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ProductList(
+              products: bloc.products,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
