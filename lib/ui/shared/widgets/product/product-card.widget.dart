1ddf0412e00e73cefc72742fb4d360e07a2eff94
Franciscpd <franciscpd@gmail.com>
2019-08-27 14:24:24 -0300

    Add product page

--- lib/ui/shared/widgets/product/product-card.widget.dart
+++ lib/ui/shared/widgets/product/product-card.widget.dart
@@ -1,6 +1,7 @@
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_bloc/models/product-list.model.dart';
+import 'package:shopping_bloc/ui/android/pages/product.page.dart';
 
 class ProductCard extends StatelessWidget {
   final ProductListModel item;
@@ -20,83 +21,95 @@ class ProductCard extends StatelessWidget {
           Radius.circular(5),
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            width: 240,
-            height: 240,
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.05),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(5),
-                topRight: Radius.circular(5),
-              ),
-              image: DecorationImage(
-                image: NetworkImage(item.image),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProductPage(
+                tag: item.tag,
               ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            padding: EdgeInsets.only(
-              left: 10,
-              right: 10
-            ),
-            height: 60,
-            child: Text(
-              item.title,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: 240,
+              height: 240,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.05),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(5),
+                  topRight: Radius.circular(5),
+                ),
+                image: DecorationImage(
+                  image: NetworkImage(item.image),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              left: 10,
-              right: 10,
+            SizedBox(
+              height: 10,
             ),
-            child: Text(
-              item.brand,
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w300,
+            Container(
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10
+              ),
+              height: 60,
+              child: Text(
+                item.title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              left: 10,
-              right: 10,
+            SizedBox(
+              height: 5,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: 120,
-                  child: Text(
-                    "R\$ ${price.format(item.price)}",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).primaryColor,
+            Padding(
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10,
+              ),
+              child: Text(
+                item.brand,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: 120,
+                    child: Text(
+                      "R\$ ${price.format(item.price)}",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor,
+                      ),
                     ),
                   ),
-                ),
-                //AddToCart(item: item),
-              ],
+                  //AddToCart(item: item),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
